3f5190345e7353d20fa731933b885bef290baa20
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-11-23 20:40:37 +0900

    ObjectPool 途中経過

---
+++ Assets/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Pool;
+
+public class Bullet : ObjectPool<Bullet>, IPool
+{
+    public void Create()
+    {
+        
+    }
+
+    public void Delete()
+    {
+
+    }
+}
---
+++ Assets/ObjectPool.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pool
+{
+    public interface IPool
+    {
+        void Create();
+        void Use();
+        void Delete();
+    }
+
+    public class ObjectPool<T> : MonoBehaviour where T : IPool
+    {
+        T GetT;
+
+        private void Awake()
+        {
+            GetT.Create();
+        }
+
+        public void Use()
+        {
+            GetT.Use();
+        }
+    }
+}
